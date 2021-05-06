6e8c60ca23fa2c879b59ad39c76a945e5b99730e
kasia-latkowska <kasia.latkowska15@gmail.com>
2021-05-06 17:56:33 -0400

    getting started and ColumnDataSource

---
+++ stage1_3.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+#from bokeh.charts import Bar
+#from bokeh.plotting import hbar
+from bokeh.plotting import ColumnDataSource, figure
+from bokeh.io import output_file, show
+
+output_file('pop-life.html')
+
+file = 'country-pops.csv'
+
+countries = pd.read_csv(file, nrows=5)
+countries_array = np.array(countries.head)
+
+#print(countries_array)
+#bar_chart = hbar(countries, 'Country_English', values='Population', title='Population', legend=False)
+#show(bar_chart)
+
+country_data = ColumnDataSource(countries)
+
+plot = figure(x_axis_label='Population', y_axis_label='Life Expectancy')
+
+plot.circle(x='Population', y='Life_expectancy', source=country_data, size=15)
+show(plot)
